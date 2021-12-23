ec42d145dd38660d205da4a7ac57b46cb0dbd5d2
AVC99 <arnauvivescomes@gmail.com>
2021-12-23 17:24:06 +0100

    Fet

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Presentation","l":"Controller"},{"p":"<Unnamed>","l":"Main"},{"p":"Presentation","l":"Menu"},{"p":"Business","l":"Product"},{"p":"Persistance","l":"ProductDAO"},{"p":"Business","l":"ProductManager"},{"p":"Business","l":"Worker"}];updateSearchResults();
